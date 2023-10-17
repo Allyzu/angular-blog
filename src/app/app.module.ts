2654ed6cae61335f3c43ce7ceddcbd5783bb17d5
Allyson Henrique <henriqueallyson18@gmail.com>
2023-10-17 05:14:33 -0300

    contact created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
